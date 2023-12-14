bc0ef56610a76be9053397186f160a164ee98be8
Robert Kosieradzki <robert.kosieradzki@gmail.com>
2023-12-14 17:36:35 +0100

    Implement Internationalization in en & pl

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withNextIntl = require('next-intl/plugin')(
+    './src/config/i18n.ts'
+);
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const config = {};
+
+module.exports = withNextIntl(config);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,63 +1,12 @@
-import type { Metadata } from 'next'
-import './globals.css'
-import Introduction from '../components/Introduction'
-// import NavBar from '../components/NavBar'
-import Experience from '../components/Experience'
-import Projects from '../components/Projects'
-// import BottomBar from '../components/BottomBar'
-import Contact from '../components/Contact'
+import {ReactNode} from 'react';
+import './globals.css';
 
+type Props = {
+  children: ReactNode;
+};
 
-export const metadata: Metadata = {
-  title: 'Portfolio Robert Kosieradzki',
-  description: 'Portfolio Robert Kosieradzki',
-}
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body className='bg-gradient-to-b from-black via-blue-900 to-blue-500 h-full bg-no-repeat bg-fixed'>
-        <div className='flex flex-row max-lg:flex-col gap-5 mt-16'>
-          <header className='lg:w-1/2'>
-            <div className='lg:w-1/2 flex flex-col justify-center items-center lg:fixed gap-3'>
-              {/* Short Introduction */}
-              <section>
-                <Introduction /> 
-              </section>
-              {/* About Me */}
-              <section className='flex text-center items-center lg:px-20 px-10'>
-                {children}
-              </section>
-              {/* Contact */}
-              <div className=''>
-                <Contact/>
-              </div>
-              {/* Navigation Links */}
-              {/* <div className='max-lg:hidden '>
-                <NavBar/>
-              </div> */}
-            </div>
-          </header>
-          <main className='lg:w-1/2 px-10 flex flex-col gap-5'>
-            {/* Experience */}
-            <section>
-              <Experience/>
-            </section>
-            {/* Projects */}
-            <section className='max-lg:mb-16'>
-              <Projects/>
-            </section>
-          </main>
-          {/* BottomBar to easy navigate on mobile devices */}
-          {/* <div className='lg:hidden fixed bottom-0 w-full'>
-            <BottomBar/>
-          </div> */}
-        </div>
-      </body>
-    </html>
-  )
+// Since we have a `not-found.tsx` page on the root, a layout file
+// is required, even if it's just passing children through.
+export default function RootLayout({children}: Props) {
+  return children;
 }
